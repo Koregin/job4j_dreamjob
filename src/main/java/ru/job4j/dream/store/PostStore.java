40d2689af7fb60c6b54a3f4f8e9a53964af20582
Evgeny Koregin <mnemonic78@yandex.ru>
2022-04-13 12:54:58 +0300

    3. PostController.createPost. Создание вакансии. [#504849]

--- src/main/java/ru/job4j/dream/store/PostStore.java
+++ src/main/java/ru/job4j/dream/store/PostStore.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PostStore {
 
@@ -13,12 +14,14 @@ public class PostStore {
 
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
+    private final AtomicInteger counter = new AtomicInteger(0);
+
     private PostStore() {
-        posts.put(1, new Post(1, "Junior Java Job",
+        posts.put(1, new Post(counter.incrementAndGet(), "Junior Java Job",
                 "Job for junior", LocalDate.of(2022, 4, 12)));
-        posts.put(2, new Post(2, "Middle Java Job",
+        posts.put(2, new Post(counter.incrementAndGet(), "Middle Java Job",
                 "Job for middle", LocalDate.of(2022, 4, 12)));
-        posts.put(3, new Post(3, "Senior Java Job",
+        posts.put(3, new Post(counter.incrementAndGet(), "Senior Java Job",
                 "Job for senior", LocalDate.of(2022, 4, 12)));
     }
 
@@ -28,5 +31,11 @@ public class PostStore {
 
     public Collection<Post> findAll() {
         return posts.values();
+    }
+
+    public Post add(Post post) {
+        post.setId(counter.incrementAndGet());
+        post.setCreated(LocalDate.now());
+        return posts.put(post.getId(), post);
     }
 }
