8d879fa78a9a991a1a6d42c4109217932bd0ccd5
Evgeny Koregin <mnemonic78@yandex.ru>
2022-04-18 11:44:04 +0300

    4. Многопользовательские приложения. Многопоточность в Веб приложении. [#504857]

--- src/main/java/ru/job4j/dream/service/CandidateService.java
+++ src/main/java/ru/job4j/dream/service/CandidateService.java
@@ -1,5 +1,6 @@
 package ru.job4j.dream.service;
 
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Service;
 import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.store.CandidateStore;
@@ -7,6 +8,7 @@ import ru.job4j.dream.store.CandidateStore;
 import java.util.Collection;
 
 @Service
+@ThreadSafe
 public class CandidateService {
     private final CandidateStore store;
 
