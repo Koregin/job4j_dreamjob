6dd85087d9c56b199032f1182cfcb8e4fb796489
Evgeny Koregin <mnemonic78@yandex.ru>
2022-04-19 12:34:07 +0300

    3. Формы. Загрузка файла на сервер. [#504855]

--- src/main/java/ru/job4j/dream/controller/CandidateController.java
+++ src/main/java/ru/job4j/dream/controller/CandidateController.java
@@ -1,30 +1,34 @@
 package ru.job4j.dream.controller;
 
 import net.jcip.annotations.ThreadSafe;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.service.CandidateService;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 @Controller
 @ThreadSafe
 public class CandidateController {
 
-    private final CandidateService service;
+    private final CandidateService candidateService;
 
     public CandidateController(CandidateService service) {
-        this.service = service;
+        this.candidateService = service;
     }
 
     @GetMapping("/candidates")
     public String candidates(Model model) {
-        model.addAttribute("candidates", service.findAll());
+        model.addAttribute("candidates", candidateService.findAll());
         return "candidates";
     }
 
@@ -35,20 +39,38 @@ public class CandidateController {
     }
 
     @PostMapping("/createCandidate")
-    public String createCandidate(@ModelAttribute Candidate candidate) {
-        service.create(candidate);
+    public String createCandidate(@ModelAttribute Candidate candidate,
+                                  @RequestParam("file") MultipartFile file) throws IOException {
+        candidate.setPhoto(file.getBytes());
+        candidateService.create(candidate);
         return "redirect:/candidates";
     }
 
     @GetMapping("/formUpdateCandidate/{candidateId}")
     public String formUpdateCandidate(Model model, @PathVariable("candidateId") int id) {
-        model.addAttribute("candidate", service.findById(id));
+        model.addAttribute("candidate", candidateService.findById(id));
         return "updateCandidate";
     }
 
     @PostMapping("/updateCandidate")
-    public String updateCandidate(@ModelAttribute Candidate candidate) {
-        service.update(candidate);
+    public String updateCandidate(@ModelAttribute Candidate candidate,
+                                  @RequestParam("file") MultipartFile file) throws IOException {
+        if (!file.isEmpty()) {
+            candidate.setPhoto(file.getBytes());
+        } else {
+            candidate.setPhoto(candidateService.findById(candidate.getId()).getPhoto());
+        }
+        candidateService.update(candidate);
         return "redirect:/candidates";
+    }
+
+    @GetMapping("/photoCandidate/{candidateId}")
+    public ResponseEntity<Resource> download(@PathVariable("candidateId") Integer candidateId) {
+        Candidate candidate = candidateService.findById(candidateId);
+        return ResponseEntity.ok()
+                .headers(new HttpHeaders())
+                .contentLength(candidate.getPhoto().length)
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(new ByteArrayResource(candidate.getPhoto()));
     }
 }
